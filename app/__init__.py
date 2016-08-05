8d905cc8cc69d88031b4fa65bb13009e0fdfaff1
kaeunlee <likalella95@gmail.com>
2016-08-05 18:58:17 +0900

    fix about Blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,29 @@
-import sys
-
-from flask import Flask
+from flask import Flask, url_for
 from flask.ext.marshmallow import Marshmallow
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
 from config import config
 
-
+app = Flask(__name__)
 db = SQLAlchemy()
 ma = Marshmallow()
 
 from app.models import *
+from app.routes import *
+from app.api_v1 import *
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://mewmew:mewmew16@localhost/mewmew'
 
     db.init_app(app)
     db.app=app
     ma.init_app(app)
-
     db.create_all()
 
-    from .api_v1 import api as api_v1_blueprint
-    app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(blueprint)
 
     return app
