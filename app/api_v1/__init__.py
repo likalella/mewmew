e8fa2c9cca170e0e2c83f566e8658e670eca6920
kaeunlee <likalella95@gmail.com>
2016-08-05 19:00:08 +0900

    fix to use flask restful with blueprint.

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -1,8 +1,16 @@
 from flask import Blueprint
+from flask_restful import Api
 
+from app import app
 
-api = Blueprint('api_v1', __name__)
+from app.api_v1.document import Document
+from app.api_v1.photo import Photo
+from app.api_v1.user import User
 
+api = Api()
+blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+api.init_app(blueprint)
 
-# Import any endpoints here to make them available
-#from . import dis_endpoint, dat_endpoint
+api.add_resource(Document, '/documents/<int:document_id>')
+api.add_resource(Photo, '/photos/<int:photo_id>')
+api.add_resource(User, '/users/<int:user_id>')
